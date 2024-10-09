da27222dccac2b33638e887897b5f3173f32735e
Osmane Barbosa Neto <135390417+OsmaneNeto@users.noreply.github.com>
2024-10-09 17:22:53 -0300

    Add files via upload

---
+++ logging.js
@@ -0,0 +1,10 @@
+//middleware
+const logging = (req, res, next)=>{
+    const now = new Date();
+    console.log(now, req.method, req.originalUrl);
+   
+    next();
+};
+
+
+module.exports = logging;
